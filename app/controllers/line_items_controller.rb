8ab850b616b2cda1c1f7852973fde94155058d5b
Simon Stubben <simonstubben@gmail.com>
2015-11-18 11:12:01 +0800

    Changes..

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,7 +30,7 @@ class LineItemsController < ApplicationController
     product = Product.find(params[:product_id])
     session[:counter] = 0
 
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
